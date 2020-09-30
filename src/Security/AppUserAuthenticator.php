375e6266cef0151386ddcb645c8e6ee0db87fd37
JimHin <krowstudio@icloud.com>
2020-09-30 19:18:26 +0200

    Réglage du formulaire d'enregistrement avec le nom de l'équipe

--- src/Security/AppUserAuthenticator.php
+++ src/Security/AppUserAuthenticator.php
@@ -96,8 +96,8 @@ class AppUserAuthenticator extends AbstractFormLoginAuthenticator implements Pas
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('booking_index'));
+
     }
 
     protected function getLoginUrl()
